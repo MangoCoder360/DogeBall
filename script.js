d9d08d6f29d8a8e259f4a2184dc86dba9fc5e354
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-10-30 16:44:47 -0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+var PLAYER_SPEED = 10;
+var PLAYER_SIZE = 20;
+var ENEMY_SPEED = 50;
+var ENEMY_SIZE = 15;
+var playerX;
+var playerY;
+var score;
+var aKeyPressed;
+var sKeyPressed;
+var dKeyPressed;
+var wKeyPressed;
+var enemyArray = [];
+var canvasObject = document.getElementById("myCanvas")
